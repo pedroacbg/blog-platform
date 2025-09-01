e3b9a1a16e43694e799a1c4518a57a5b62591aa1
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2025-09-01 11:22:59 -0300

    implementing user details

---
+++ src/main/java/com/pedroacbg/blog/security/BlogUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.pedroacbg.blog.security;
+
+import com.pedroacbg.blog.domain.model.User;
+import com.pedroacbg.blog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class BlogUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Nenhum usuário encontrado com o email: " + email));
+        return new BlogUserDetails(user);
+    }
+}
